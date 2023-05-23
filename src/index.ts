1278e1a148a48dfe5b566429ec4508abb09a70f9
Haider <haider.salar@gmail.com>
2023-05-23 23:05:57 +0530

    practice

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-console.log("hello world")
+console.log("hello world");
 
-let age: number = 10
+let age: number = 10;
+let nameOne: string = "haider";
+let is_published: boolean = true;
+let num: any = 123_456_789
+let level;
+
+// array
+let numberss: number[]  = [1,2,3]; 
+
+// tuple: array with fixed length and fixed type of elements
+let user: [number, string] = [1, "haider"]
